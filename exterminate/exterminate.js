ed0a8cae9ac52bd8027117d7b4d877da79881222
Linkaru <30627754+Linkaru@users.noreply.github.com>
2018-04-30 23:12:50 -0400

    Update exterminate.js

--- exterminate/exterminate.js
+++ exterminate/exterminate.js
@@ -9,6 +9,57 @@ var time;
 var gui;
 var score = 0;
 
+function Fly() {
+   //sets a temporary sprite object
+   tFly = new Sprite(scene, "http://aharrisbooks.net/h5g/h5g_7/fly.png",20,20);
+   //sets the speed of the sprite object
+   tFly.setSpeed(10);
+   //create a function to simulate a fly's flight pattern.
+   tFly.wriggle = function() {
+       //random fly angle--improve this later
+       newDir = (Math.random() * 90)-45;
+       this.changeAngleBy(newDir);
+       
+   };
+   
+   tFly.reset = function(){
+       //set new random position
+       newX =  Math.random() * this.cWidth;
+       newY = Math.random() * this.cHeight;
+       this.setPosition(newX, newY);
+   };
+   tFly.reset();
+   return tFly;
+}
+
+function Frog(){
+    tFrog = new Sprite(scene, "http://aharrisbooks.net/h5g/h5g_7/frog.png", 50,50);
+    tFrog.maxSpeed = 10;
+    tFrog.minSpeed = -3;
+    tFrog.setSpeed(0);
+    tFrog.setAngle(0);
+    
+    tFrog.checkKeys = function(){
+        if(keysDown[K_LEFT]){
+            this.changeMoveAngleBy(-5);
+        }
+        if(keysDown[K_RIGHT]){
+            this.changeMoveAngleBy(5);
+        }
+        if(keysDown[K_UP]){
+            this.changeSpeedBy(1);
+        }
+        if(keysDown[K_DOWN]){
+            this.changeSpeedBy(-1);
+            if(this.speed < this.minSpeed){
+                this.setSpeed(this.minSpeed);
+            }
+        }
+    };
+    
+    return tFrog;
+}
+
 function checkCollisions(indexFly){
     if(frog.collidesWith(flies[indexFly])){
         flies[indexFly].reset();
